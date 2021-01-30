0bf8908ce4217aaeecb12b851f586a00f373805d
NicoFleurinck <73880514+NicoFleurinck@users.noreply.github.com>
2021-01-30 12:06:56 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    tijd = randint(2, 8)
+    oefeining = oefeningenlijst[randint(0, oefeningenlijst.length - 1)]
+    opdracht = "" + tijd + "s" + oefeining
+    basic.clearScreen()
+    basic.showString(opdracht)
+    basic.showNumber(tijd)
+    for (let index = 0; index < tijd; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showLeds(`
+            # # # # #
+            # # . # #
+            # . # . #
+            # # . # #
+            # # # # #
+            `)
+    }
+    basic.showIcon(IconNames.StickFigure)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("prima!")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+let opdracht = ""
+let oefeining = ""
+let tijd = 0
+let oefeningenlijst: string[] = []
+oefeningenlijst = ["koprol", "flik", "salto", "draaien"]
+basic.showIcon(IconNames.Heart)
